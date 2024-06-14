a6543875121cb8a31630a955225996396e3dd36d
Rashad Edwards <rashadedwards@macbook.local>
2024-06-14 12:28:49 -0400

    created navBar using react components, jsx, and javascript

--- src/App.js
+++ src/App.js
@@ -1,1 +1,27 @@
+export default function App() {
+  return (
+    <>
+      <NavBar />
+    </>
+  );
+}
 
+function NavBar() {
+  return (
+    <body>
+      <div className="align-header">
+        <header>
+          <h1 id="logo">Airline Meez</h1>
+          <nav>
+            <ul>
+              <li>Who Are We</li>
+              <li>Plan Travel</li>
+              <li>Travel Information</li>
+              <li>Flight Schedule</li>
+            </ul>
+          </nav>
+        </header>
+      </div>
+    </body>
+  );
+}
